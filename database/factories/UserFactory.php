e77b96ac03f4a198b791b5bd3abd961ec9739412
Mongrus <artemromanowich90@gmail.com>
2025-07-29 15:06:32 +0300

    feat(seeder): add UserFactory and UserSeeder with fake data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Enums\UserRole;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -24,10 +25,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'name' => fake()->firstName(),
+            'surname' => fake()->lastName(),
+            'login' => fake()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => fake()->randomElement([UserRole::ADMIN, UserRole::EMPLOYEE, UserRole::VISITOR,]),
             'remember_token' => Str::random(10),
         ];
     }
