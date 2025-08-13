b24de8d3467b3d99c002fcd742fb4caf147529a8
Mongrus <artemromanowich90@gmail.com>
2025-08-13 17:05:11 +0300

    Исправление ошибки пути до UserController в api.php (временно не используется)

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Web\UserController;
 
 Route::post('/register', [UserController::class, 'register'])->name('user.register');
